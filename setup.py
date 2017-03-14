18b9bc332b3dd34c5a831e60762ecfe76fa66c3b
Stefano Pedemonte <stefano.pedemonte@gmail.com>
2017-03-14 13:59:51 -0400

    Improved documentation markdown
    Improvement in the markdown, not in the content

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from glob import glob
 
 setup(
     name='occiput',
-    version='1.0.9',
+    version='1.0.11',
     author='Stefano Pedemonte',
     author_email='stefano.pedemonte@gmail.com',
     packages=['occiput', 
